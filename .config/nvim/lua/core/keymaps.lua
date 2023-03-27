854fd6934abec8d9780b87139199405dc6a7bef5
nikolay <nikolay@nemasmisal.com>
2023-03-27 21:10:33 +0300

    dotfiles

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,15 @@
+local vim = vim
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
